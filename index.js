7439f0e7550265c66d1f0c212265aa709bfa60ef
shahin009 <shaheenmis170879@gmail.com>
2024-06-06 14:15:58 +0600

    user collection running

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb');
 const port = process.env.PORT || 4009;
 
 
 // middleware
-app.use(cors({
-    origin: [
-        'http://localhost:5173'
-    ]
-}));
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
 app.use(express.json())
+app.use(cookieParser())
+
+// Verify Token Middleware
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token
+    console.log(token)
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            console.log(err)
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded
+        next()
+    })
+}
 
 
 
@@ -40,12 +60,33 @@ async function run() {
         app.post('/jwt', async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '7d'
+                expiresIn: '365d'
             })
-            res.send({ token });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: true })
+        })
+        // Logout
+        app.get('/logout', async (req, res) => {
+            try {
+                res
+                    .clearCookie('token', {
+                        maxAge: 0,
+                        secure: process.env.NODE_ENV === 'production',
+                        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                    })
+                    .send({ success: true })
+                console.log('Logout successful')
+            } catch (err) {
+                res.status(500).send(err)
+            }
         })
 
-        //save user
+        //save user data in database
         app.put('/user', async (req, res) => {
             const user = req.body;
             const query = { email: use?.email }
@@ -72,6 +113,11 @@ async function run() {
                 },
             }
             const result = await usersCollection.updateOne(query, updateDoc, options)
+            res.send(result)
+        })
+        // get all users data from db
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
             res.send(result)
         })
         // Forum API 
